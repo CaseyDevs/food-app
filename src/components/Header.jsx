9e8b615fcfd6525eb3665953d81d86ebc79d201b
Casey Page <CaseyDevs5@gmail.com>
2025-02-22 14:18:06 +0000

    Create page header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header id="main-header">
+            <div id="title">
+                <img src="../src/assets/logo.jpg" alt="ReactMeals logo" />
+                <h1>ReactMeals</h1>
+            </div>
+            <button>Cart</button>
+        </header>
+    )
+}
